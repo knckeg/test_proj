2feab8f41c2141c9c44ee7ad5c1318e12e63bbdb
Kacper <kgilbas@st.swps.edu.pl>
2025-01-02 17:18:09 +0100

    Dodanie modeli Osoba i Stanowisko

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import Team, Person
 
+from .models import Osoba, Stanowisko
+
 admin.site.register(Team)
 admin.site.register(Person)
+
+@admin.register(Stanowisko)
+class StanowiskoAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'opis')
+
+@admin.register(Osoba)
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ('imie', 'nazwisko', 'plec', 'stanowisko')
