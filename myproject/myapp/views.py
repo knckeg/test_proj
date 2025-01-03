1aa645db32a2de016de9631e1448d2da822d223d
Kacper <kgilbas@st.swps.edu.pl>
2025-01-03 18:50:06 +0100

    chuj18

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Osoba, Stanowisko
+from .serializers import OsobaSerializer, StanowiskoSerializer
+from rest_framework.filters import SearchFilter
+
+class OsobaViewSet(viewsets.ModelViewSet):
+    queryset = Osoba.objects.all()
+    serializer_class = OsobaSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ['imie', 'nazwisko']
+
+    @action(detail=True, methods=['delete'])
+    def remove(self, request, pk=None):
+        osoba = self.get_object()
+        osoba.delete()
+        return Response({"message": "Osoba została usunięta"})
+
+class StanowiskoViewSet(viewsets.ModelViewSet):
+    queryset = Stanowisko.objects.all()
+    serializer_class = StanowiskoSerializer
+
+    @action(detail=True, methods=['delete'])
+    def remove(self, request, pk=None):
+        stanowisko = self.get_object()
+        stanowisko.delete()
+        return Response({"message": "Stanowisko zostało usunięte"})
+
