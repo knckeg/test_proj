63f72484ad5cbe1b2bbe5004419a5853da4bcd17
Kacper <kgilbas@st.swps.edu.pl>
2025-01-03 18:07:24 +0100

    chuj13

---
+++ myproject/myapp/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+
+from myapp.models import Team, Stanowisko, Osoba
+
+class OsobaSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    imie = serializers.CharField(max_length=50)
+    nazwisko = serializers.CharField(max_length=50)
+    plec = serializers.ChoiceField(choices=[
+        (1, 'Kobieta'),
+        (2, 'Mężczyzna'),
+        (3, 'Inna'),
+    ])
+    stanowisko = serializers.CharField(source='stanowisko.nazwa', read_only=True)
+
+    def create(self, validated_data):
+        """
+        Tworzy nowy obiekt Osoba na podstawie danych wejściowych.
+        """
+        from myapp.models import Osoba  # Zaimportuj model Osoba
+        return Osoba.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Aktualizuje istniejący obiekt Osoba na podstawie danych wejściowych.
+        """
+        instance.imie = validated_data.get('imie', instance.imie)
+        instance.nazwisko = validated_data.get('nazwisko', instance.nazwisko)
+        instance.plec = validated_data.get('plec', instance.plec)
+        instance.save()
+        return instance
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'name', 'country']
+
+class StanowiskoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stanowisko
+        fields = ['id', 'nazwa', 'opis']
+
+class OsobaModelSerializer(serializers.ModelSerializer):
+    stanowisko = serializers.StringRelatedField()  # Wyświetla __str__() z modelu Stanowisko
+
+    class Meta:
+        model = Osoba
+        fields = ['id', 'imie', 'nazwisko', 'plec', 'data_dodania', 'stanowisko']
+
