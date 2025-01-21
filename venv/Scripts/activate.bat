0dc8ef21a2398af328005d279b6f41a9d830d8d6
Kacper <kgilbas@st.swps.edu.pl>
2025-01-21 19:28:14 +0100

    :(

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Homework\i hate this\test_proj\venv
+set VIRTUAL_ENV=C:\Homework\solo bolo\test_proj\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
